4a7a331567e11720c0fdd3b9bf7c5cabf7fcd9a1
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-08-26 11:49:49 +0200

    removed useless configs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svg from "vite-plugin-svgo";
+import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svg()],
+  plugins: [
+    react(),
+    // svgr({
+    //   svgrOptions: {
+    //     plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"], // Включаем SVGO и JSX-плагин
+    //     svgoConfig: {
+    //       // Здесь укажите конфигурацию SVGO, например:
+    //       multipass: true, // Многопроходная оптимизация
+    //       plugins: [
+    //         { name: "preset-default" }, // Стандартные плагины SVGO
+    //         { name: "removeDimensions", active: true }, // Пример: удаление размеров
+    //         // Добавьте другие плагины SVGO по необходимости (см. документацию SVGO)
+    //       ],
+    //     },
+    //   },
+    // }),
+  ],
 });
